b9ee402e0ceebb70f8698292ca3055fe7723c737
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2022-09-10 15:20:40 -0300

    Staff update
    Changed staff  and fireball sprites, added shooting mechanic, added particles for explosion, fixed staff rotation.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    public GameObject explosion;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
