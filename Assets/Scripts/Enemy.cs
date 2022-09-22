2926f78504644dd4c83f427f8c3f5044a8e07f9d
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2022-09-21 21:54:14 -0300

    Enemy Healthbars Implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public int health;
 
+    public int maxHealth;
+
     [HideInInspector]
     public Transform player;
 
@@ -15,13 +17,18 @@ public class Enemy : MonoBehaviour
 
     public int damage;
 
+    public HealthBarBehaviour healthBar;
+
+
     public virtual void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        healthBar.SetHealth(health, maxHealth);
     }
     public void TakeDamage(int damageAmount)
     {
         health -= damageAmount;
+        healthBar.SetHealth(health, maxHealth);
 
         if (health <= 0)
         {
